4b63b35e7d6b27e493abb3558b08c447a14a30b1
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-11-04 19:04:24 +0600

    initialModal

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const InitialModal = () => {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
