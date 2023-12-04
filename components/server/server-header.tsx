e2a6d19a98fe91accdb92adc93df5021a94f4f39
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-12-04 22:34:02 +0600

    leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -83,7 +83,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             </DropdownMenuItem>
           )}
           {!isAdmin && (
-            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            <DropdownMenuItem
+              onClick={() => onOpen("leaveServer", { server })}
+              className="px-3 py-2 text-sm cursor-pointer"
+            >
               Leave Server
               <LogOut className="text-rose-500 h-4 w-4 ml-auto " />
             </DropdownMenuItem>
